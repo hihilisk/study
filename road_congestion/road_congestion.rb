612976ec61bfac28c27b400f1ce19454c931b683
hihilisk <hihilisk@yandex.ru>
2019-07-08 12:23:55 +0300

    Renamed class 'RoadCongestion' to 'RoadLoadChecker'.
    Method 'initialize' takes an array instead of a string.

---
+++ road_congestion/road_congestion.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class RoadLoadChecker
+  def initialize(array_of_dots)
+    @array_car_position = array_of_dots
+  end
+
+  def show_road_load_in_points
+    "road congestion: #{calculate_road_congestion_in_points}"
+  end
+
+  private
+
+  def calculate_distance_between_car
+    distance_between_car = []
+
+    @array_car_position.each_index do |i|
+      if i < @array_car_position.size - 1
+        distance_between_car.push((@array_car_position[i] - @array_car_position[i + 1]).abs)
+      else
+        break
+      end
+    end
+
+    distance_between_car
+  end
+
+  def calculate_average_distance_between_car
+    acc = 0.0
+    calculate_distance_between_car.each { |i| acc += i }
+    acc / calculate_distance_between_car.size
+  end
+
+  def calculate_road_congestion_in_points
+    road_congestion = 10 - calculate_average_distance_between_car / 10
+    road_congestion < 0 ? 0 : road_congestion.round(1)
+  end
+end
