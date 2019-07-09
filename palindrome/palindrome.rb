56612167310e53a77d2b13074262d8fd13a33da9
hihilisk <hihilisk@yandex.ru>
2019-07-09 12:21:57 +0300

    refactoring code

--- palindrome/palindrome.rb
+++ palindrome/palindrome.rb
@@ -1,16 +1,16 @@
-def palindrome(string)
+check_palindrome = ->(string) {
   if string == string.reverse
     'It`s a palindrome'
   else
     'It`s not a palindrome'
   end
+}
+
+def palindrome(string, check_palindrome)
+  check_palindrome.call(string)
 end
 
-def palindrome_no_spaces(string)
-  string_no_spaces = string.gsub(' ', '')
-  if string_no_spaces == string_no_space.reverse
-    'It`s a palindrome'
-  else
-    'It`s not a palindrome'
-  end
+def palindrome_no_spaces(string, check_palindrome)
+  string = string.gsub(' ', '')
+  check_palindrome.call(string)
 end
