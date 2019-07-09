8ce2c59a07feb9fbd012b59ff0584cd29a791f25
hihilisk <hihilisk@yandex.ru>
2019-07-09 16:56:48 +0300

    refactoring code

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,7 +1,8 @@
-function palindrome (string) {
-  if (string === string.split("").reverse().join("")) {
+function palindrome(string) {
+  let stringReplaceSpace=string.replace(/\s/g,'');
+  if (string===string.split('').reverse().join('')) {
       return 'It`s a palindrome';
-  } else if (string.replace(/\s/g,'') === string.replace(/\s/g,'').split("").reverse().join("") ) {
+  } else if (stringReplaceSpace===stringReplaceSpace.split('').reverse().join('')) {
       return 'It`s a palindrome, no spaces';
   } else {
       return 'It`s not a palindrome';
