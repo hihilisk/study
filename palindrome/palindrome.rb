7feecec343480155141187e011fa367d97a1e74d
hihilisk <hihilisk@yandex.ru>
2019-07-09 14:16:53 +0300

    refactoring code v2

--- palindrome/palindrome.rb
+++ palindrome/palindrome.rb
@@ -1,16 +1,9 @@
-check_palindrome = ->(string) {
+def palindrome(string)
   if string == string.reverse
     'It`s a palindrome'
+  elsif string.gsub(' ', '') == string.reverse.gsub(' ', '')
+    'It`s a palindrome, no spaces'
   else
     'It`s not a palindrome'
   end
-}
-
-def palindrome(string, check_palindrome)
-  check_palindrome.call(string)
-end
-
-def palindrome_no_spaces(string, check_palindrome)
-  string = string.gsub(' ', '')
-  check_palindrome.call(string)
 end
