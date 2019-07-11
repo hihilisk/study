53c494b14120b7ffc0900d2d0639ad70151fe094
hihilisk <hihilisk@yandex.ru>
2019-07-11 13:18:47 +0300

    refactori sort_bubbles.js he`s work

--- sort_bubbles/sort_bubbles.js
+++ sort_bubbles/sort_bubbles.js
@@ -1,15 +1,14 @@
 function sortArray(unsortedArray) {
-  for (let j = 0; j < unsortedArray.length - 1; j + 1){
-    for (let i = 0; i < j; i + 1) {
-      let currentElement = unsortedArray[i];
-      let nextElement = unsortedArray[i+ 1];
+    let sortedArray= unsortedArray;
+    let arrayLength = sortedArray.length -1;
 
-      if (currentElement > nextElement) {
-        let temp = currentElement;
-        currentElement= nextElement;
-        nextElement = temp;
-        console.log(unsortedArray);
-      }
+    for (let j = 0; j < arrayLength; j + 1){
+        for (let i = 0; i < arrayLength; i + 1) {
+
+            if (sortedArray[i] > sortedArray[i +1]) {
+                sortedArray[i], sortedArray[i + 1] = sortedArray[i + 1], sortedArray[i];
+            }
+        }
     }
-  }
+    return sortedArray
 }
