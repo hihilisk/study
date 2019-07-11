0cc0573d14bb444866f407a0773b5fdecef79014
hihilisk <hihilisk@yandex.ru>
2019-07-11 13:16:43 +0300

    refactori sort_bubbles.js he`s work

---
+++ sort_bubbles/sort_bubbles.js
@@ -0,0 +1,14 @@
+function sortArray(unsortedArray) {
+  let sortedArray= unsortedArray;
+  let arrayLength = sortedArray.length -1;
+
+  for (let j = 0; j < arrayLength; j + 1){
+    for (let i = 0; i < arrayLength; i + 1) {
+
+      if (sortedArray[i] > sortedArray[i +1]) {
+        sortedArray[i], sortedArray[i + 1] = sortedArray[i + 1], sortedArray[i];
+      }
+    }
+  }
+  return sortedArray
+}
