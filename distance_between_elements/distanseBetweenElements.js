ff9d2cbeb63e2be06bf61d6dc8e2a85ba92b12b2
hihilisk <hihilisk@yandex.ru>
2019-07-16 17:02:45 +0300

    corrected the behavior of the function when called with an argument once in the array

--- distance_between_elements/distanseBetweenElements.js
+++ distance_between_elements/distanseBetweenElements.js
@@ -2,7 +2,7 @@ function betweenDistance(array,item) {
   let distanceArray = [];
   let maxRange = 'элемент ' + item + ' не найден, или массив пуст';
 
-  for (let j = 0; j < array.length - 1; j++) {
+  for (let j = 0; j < array.length; j++) {
     if (item === array[j]) {
       distanceArray.push(j);
     }
