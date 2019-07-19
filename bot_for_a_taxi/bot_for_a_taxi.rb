6520d158016ee8b09623cb98d80b0a4105c85c43
hihilisk <hihilisk@yandex.ru>
2019-07-19 23:07:20 +0300

    refactoring code:   -delete method "request_average_speed"   -crate const 'AVERAGE_SPEED'     choice_taxi_car   -rename unused variable '_key' to 'key'     calculate_coast   -randomized value variable 'rate' refactoring methods: calculate_time_waiting and calculate_time_trip  -changes made for use new const 'AVERAGE_SPEED'

--- bot_for_a_taxi/bot_for_a_taxi.rb
+++ bot_for_a_taxi/bot_for_a_taxi.rb
@@ -1,12 +1,12 @@
 class TaxiToolkit
   attr_reader :from, :into, :time_waiting, :time_trip, :taxi_car, :coast, :distance_trip
+  AVERAGE_SPEED = 60.0
 
   def initialize(from, into)
     @from = from
     @into = into
     calculate_distance
     choice_taxi_car
-    request_average_speed
     calculate_coast
     calculate_time_trip
     calculate_time_waiting
@@ -15,18 +15,18 @@ class TaxiToolkit
   private
 
   def calculate_time_waiting
-    @time_waiting = @taxi_car[:distance] / @average_speed
+    @time_waiting = @taxi_car[:distance] / AVERAGE_SPEED
     @time_waiting = "#{@time_waiting.floor}h. #{((@time_waiting - @time_waiting.floor) * 60).round}min."
   end
 
   def calculate_time_trip
-    @time_trip = @distance_trip.to_i / @average_speed
+    @time_trip = @distance_trip.to_i / AVERAGE_SPEED
     @time_trip = "#{@time_trip.floor}h. #{((@time_trip - @time_trip.floor) * 60).round}min."
   end
 
   def calculate_coast
-    rate = 8
-    @coast = "#{(@distance_trip.to_i + @taxi_car[:distance]) / rate} USD"
+    rate = (rand * 10).round(2)
+    @coast = "#{((@distance_trip.to_i + @taxi_car[:distance]) / rate).round(2)} USD"
   end
 
   def calculate_distance
@@ -34,16 +34,12 @@ class TaxiToolkit
   end
 
   def choice_taxi_car
-    taxi_car_location = { 1 => { model: 'Hyundai solaris', color: 'серая', number: 'x161xx161', distance: 5 },
-                          2 => { model: 'Lada Kalina', color: 'черная', number: 'x162xx162', distance: 10 },
-                          3 => { model: 'Lada Granta', color: 'белая', number: 'x163xx163', distance: 15 } }
+    taxi_car_location = { 1 => { model: 'Hyundai solaris', color: 'grey', number: 'x161xx161', distance: 5 },
+                          2 => { model: 'Lada Kalina', color: 'black', number: 'x162xx162', distance: 10 },
+                          3 => { model: 'Lada Granta', color: 'white', number: 'x163xx163', distance: 15 } }
 
-    @taxi_car = taxi_car_location.each { |_key, car| car[:distance] = (car[:distance] - @from.to_i).abs }
-    @taxi_car = @taxi_car.min_by { |_key, car| car[:distance] }
+    @taxi_car = taxi_car_location.each { |key, car| car[:distance] = (car[:distance] - @from.to_i).abs }
+    @taxi_car = @taxi_car.min_by { |key, car| car[:distance] }
     @taxi_car = @taxi_car.flatten[1]
-  end
-
-  def request_average_speed
-    @average_speed = 60.0
   end
 end
