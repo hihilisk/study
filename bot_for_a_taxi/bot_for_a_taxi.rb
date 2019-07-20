b37a4327a349486b353c50476f98f291c08b5ac9
hihilisk <hihilisk@yandex.ru>
2019-07-20 21:18:40 +0300

    refactoring code:     initialize   -result calculate "@distance_trip" pasted in string     price   -calculated cost and pasting result to string   -removed method 'calculate_coast'   -added access to variable "@distance" with method 'attr_reader'

--- bot_for_a_taxi/bot_for_a_taxi.rb
+++ bot_for_a_taxi/bot_for_a_taxi.rb
@@ -1,18 +1,16 @@
 class TaxiBot
+  attr_reader :distance
   AVERAGE_SPEED = 60
 
   def initialize(point_a, point_b)
     @point_a = point_a
     @point_b = point_b
-    @distance_trip = (@point_a - @point_b).abs
+    @distance = "#{(@point_a - @point_b).abs} km"
   end
 
   def price
-    "#{calculate_coast}$"
-  end
-
-  def distance
-    "#{@distance_trip} km"
+    rate = (rand * 10).round(2)
+    @coast = "#{((select_car[:distance] + @distance.to_i) / rate).round(2)} $"
   end
 
   def waiting_car_time
@@ -20,17 +18,13 @@ class TaxiBot
   end
 
   private
+
   def select_car
-    taxi_car_location = [ { model: 'Hyundai solaris', color: 'grey', number: 'x161xx161', distance: 15 },
-                          { model: 'Lada Kalina', color: 'black', number: 'x162xx162', distance: 25 },
-                          { model: 'Lada Granta', color: 'white', number: 'x163xx163', distance: 35 } ]
+    taxi_car_location = [{ model: 'Hyundai solaris', color: 'grey', number: 'x161xx161', distance: 15 },
+                         { model: 'Lada Kalina', color: 'black', number: 'x162xx162', distance: 25 },
+                         { model: 'Lada Granta', color: 'white', number: 'x163xx163', distance: 35 }]
 
     @taxi_car = taxi_car_location.each { |car| car[:distance] = (car[:distance] - @point_a.to_i).abs }
     @taxi_car = @taxi_car.min_by { |car| car[:distance] }
-  end
-
-  def calculate_coast
-    rate = (rand * 10).round(2)
-    @coast = ((select_car[:distance] + @distance_trip.to_i) / rate).round(2)
   end
 end
