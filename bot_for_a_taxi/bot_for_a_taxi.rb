f9086254adb19c6e21f951bb705a110db1321b58
hihilisk <hihilisk@yandex.ru>
2019-07-20 14:27:17 +0300

    refactoring code:     initialize   -deleted all calling methods   -add calculate "@distance_trip"     new methods   -price: calling private method 'calculate_coast' and pasting result in string   -distance: calling variable '@distance_prim' and pasting result in string   -waiting_car_time: calling private method 'select_car' with key [:distance], const 'AVERAGE_SPEED' and pasting result in string     calculate_coast and select_car   -moving in private scope

--- bot_for_a_taxi/bot_for_a_taxi.rb
+++ bot_for_a_taxi/bot_for_a_taxi.rb
@@ -1,44 +1,36 @@
-class TaxiToolkit
-  attr_reader :from, :into, :time_waiting, :time_trip, :taxi_car, :coast, :distance_trip
-  AVERAGE_SPEED = 60.0
+class TaxiBot
+  AVERAGE_SPEED = 60
 
-  def initialize(from, into)
-    @from = from
-    @into = into
-    calculate_distance
-    choice_taxi_car
-    calculate_coast
-    calculate_time_trip
-    calculate_time_waiting
+  def initialize(point_a, point_b)
+    @point_a = point_a
+    @point_b = point_b
+    @distance_trip = (@point_a - @point_b).abs
+  end
+
+  def price
+    "#{calculate_coast}$"
+  end
+
+  def distance
+    "#{@distance_trip} km"
+  end
+
+  def waiting_car_time
+    "#{select_car[:distance] / AVERAGE_SPEED * 60} minutes"
   end
 
   private
+  def select_car
+    taxi_car_location = [ { model: 'Hyundai solaris', color: 'grey', number: 'x161xx161', distance: 15 },
+                          { model: 'Lada Kalina', color: 'black', number: 'x162xx162', distance: 25 },
+                          { model: 'Lada Granta', color: 'white', number: 'x163xx163', distance: 35 } ]
 
-  def calculate_time_waiting
-    @time_waiting = @taxi_car[:distance] / AVERAGE_SPEED
-    @time_waiting = "#{@time_waiting.floor}h. #{((@time_waiting - @time_waiting.floor) * 60).round}min."
-  end
-
-  def calculate_time_trip
-    @time_trip = @distance_trip.to_i / AVERAGE_SPEED
-    @time_trip = "#{@time_trip.floor}h. #{((@time_trip - @time_trip.floor) * 60).round}min."
+    @taxi_car = taxi_car_location.each { |car| car[:distance] = (car[:distance] - @point_a.to_i).abs }
+    @taxi_car = @taxi_car.min_by { |car| car[:distance] }
   end
 
   def calculate_coast
     rate = (rand * 10).round(2)
-    @coast = "#{((@distance_trip.to_i + @taxi_car[:distance]) / rate).round(2)} USD"
-  end
-
-  def calculate_distance
-    @distance_trip = "#{(@from - @into).abs} km"
-  end
-
-  def choice_taxi_car
-    taxi_car_location = [ { model: 'Hyundai solaris', color: 'grey', number: 'x161xx161', distance: 5 },
-                          { model: 'Lada Kalina', color: 'black', number: 'x162xx162', distance: 10 },
-                          { model: 'Lada Granta', color: 'white', number: 'x163xx163', distance: 15 } ]
-
-    @taxi_car = taxi_car_location.each { |car| car[:distance] = (car[:distance] - @from.to_i).abs }
-    @taxi_car = @taxi_car.min_by { |car| car[:distance] }
+    @coast = ((select_car[:distance] + @distance_trip.to_i) / rate).round(2)
   end
 end
