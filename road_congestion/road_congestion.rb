5a58a9e8c5d2a5ea61090346bba4970810007a55
hihilisk <hihilisk@yandex.ru>
2019-07-27 15:37:06 +0300

    Rename metghods and refacroting method 'calculate_distance_between_cars'

--- road_congestion/road_congestion.rb
+++ road_congestion/road_congestion.rb
@@ -1,38 +1,37 @@
 # frozen_string_literal: true
 
 class RoadLoadChecker
-  def initialize(array_of_dots)
-    @array_car_position = array_of_dots
+  def initialize(array_of_points)
+    @array_car_position = array_of_points
   end
 
   def show_road_load_in_points
-    "road congestion: #{calculate_road_congestion_in_points}"
+    calculate_road_congestion_in_points
   end
 
   private
 
-  def calculate_distance_between_car
-    distance_between_car = []
+  def calculate_distance_between_cars
+    distance_between_cars = []
 
-    @array_car_position.each_index do |i|
-      if i < @array_car_position.size - 1
-        distance_between_car.push((@array_car_position[i] - @array_car_position[i + 1]).abs)
-      else
-        break
-      end
+    @array_car_position.each do |i|
+    break if i > @array_car_position.size - 1
+
+      distance_between_two_cars = (@array_car_position[i] - @array_car_position[i + 1]).abs
+      distance_between_cars.push(distance_between_two_cars)
     end
 
-    distance_between_car
+    distance_between_cars
   end
 
-  def calculate_average_distance_between_car
+  def calculate_average_distance_between_cars
     acc = 0.0
-    calculate_distance_between_car.each { |i| acc += i }
-    acc / calculate_distance_between_car.size
+    calculate_distance_between_cars.each { |i| acc += i }
+    acc / calculate_distance_between_cars.size
   end
 
   def calculate_road_congestion_in_points
-    road_congestion = 10 - calculate_average_distance_between_car / 10
+    road_congestion = 10 - calculate_average_distance_between_cars / 10
     road_congestion < 0 ? 0 : road_congestion.round(1)
   end
 end
